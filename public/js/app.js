d218a0707f24dd2844342e92e65d2e555bb73b82
jonabasque <jonmultimedia@gmail.com>
2015-03-08 17:44:21 +0100

    Añadido bootstrap y un esqueleto de pagina principal

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,16 @@
     //Declaramos una variable con un modulo como primer parametro y un array con las dependencias como segundo.
     var app = angular.module('pokedex', []);
 
+    //declaramos el controlador del mñódulo
     app.controller('PokemonController', function(){
-
+        //El controlador tendrá acceso a un objeto llamado pokemon
         this.pokemon = {
             id: 001,
-            name: 'bulbasaur',
+            name: 'Bulbasaur',
             species : 'Seed Pokemon',
             type: ['Graas', 'Poison'],
             height: '2,4',
-            widht: '15.2 lbs',
+            weight: '15.2 lbs',
             abilities: ['Overgrow', 'Chlorophyll']
 
         };
