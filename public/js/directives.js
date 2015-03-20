081ad8c9c6b06461c077ebbb9380448311204bbe
jonabasque <jonmultimedia@gmail.com>
2015-03-20 02:32:41 +0100

    Corregidos varios errores acumulados

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,64 +1,68 @@
-(function(){
+(function () {
 
-    angular.module('pokedex.directives', [])
-        .directive('pokemonData', function(){
+  angular.module('pokedex.directives', [])
+    .directive('pokemonName', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-name.html'
+      };
+    })
 
-            return {
-                //Hay varios tipos de directivas, está será de tipo elemento.
-                restrict: 'E',
-                templateUrl: 'partials/pokemon-data.html'
-            };
-        })
-        .directive('pokemonImage', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'partials/pokemon-image.html'
-            };
-        })
-        .directive('pokemonName', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'partials/pokemon-name.html'
-            };
-        })
-        .directive('pokemonStats', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'partials/pokemon-stats.html'
-            };
-        })
-        .directive('pokemonEvolution', function () {
-            return {
-                retrict: 'E',
-                templateUrl: 'partials/pokemon-evolution.html'
-            };
-        })
-        .directive('pokemonComments', function () {
-            return {
-                retrict: 'E',
-                templateUrl: 'partials/pokemon-comments.html',
-                controller: function () {
-                  this.comments = [];
-                  this.comment = {};
-                  this.show = false;
+    .directive('pokemonImage', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-image.html'
+      };
+    })
 
-                  this.toggle = function () {
-                    this.show = !this.show;
-                  };
+    .directive('pokemonData', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-data.html'
+      };
+    })
 
-                  this.anonymousChanged = function () {
-                    if (this.comment.anonymous) {
-                      this.comment.email = "";
-                    }
-                  };
+    .directive('pokemonStats', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-stats.html'
+      };
+    })
 
-                  this.addComment = function () {
-                    this.comment.date = Date.now();
-                    this.comments.push(this.comment);
-                    this.comment = {};
-                  };
-                },
-                controllerAs: 'cmtsCtrl'
-            };
-        });
-}();
+    .directive('pokemonEvolution', function () {
+      return {
+        retrict: 'E',
+        templateUrl: 'partials/pokemon-evolution.html'
+      };
+    })
+
+    .directive('pokemonComments', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-comments.html',
+        controller: function () {
+          this.comments = [];
+          this.comment = {};
+          this.show = false;
+
+          this.toggle = function () {
+            this.show = !this.show;
+          };
+
+          this.anonymousChanged = function () {
+            if (this.comment.anonymous) {
+              this.comment.email = "";
+            }
+          };
+
+          this.addComment = function () {
+            this.comment.date = Date.now();
+            this.comments.push(this.comment);
+            this.comment = {};
+          };
+        },
+        controllerAs: 'cmtsCtrl'
+      };
+    });
+
+})();
