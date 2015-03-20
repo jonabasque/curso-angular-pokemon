af0c7d5d0c841e73c1351a2064c4f908e1700f0e
jonabasque <jonmultimedia@gmail.com>
2015-03-20 06:13:24 +0100

    Creado filtro para normalizar los simbolos

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,6 +1,17 @@
 (function () {
 
   angular.module('pokedex.filters', [])
+    //vamos a crear un filtro para reemplazar los simbolos por el caracter que tienen en la imagen.
+    .filter('normalize', function(){
+        return function(input){
+            input = input
+                .replace('♀', 'f')
+                .replace('♂', 'm')
+                .replace(/\W+/g, '');
+                
+            return input.toLowerCase();
+        };
+    })
     .filter('imageify', function () {
       return function (input) {
         var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
